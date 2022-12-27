f9b186b899e0da7ea3660d4695e81f87c5727177
Martin <martinmarescr@gmail.com>
2022-12-27 15:22:56 -0600

    Index updated

--- react-portfolio/pages/index.tsx
+++ react-portfolio/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-
-
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillTwitterCircle,AiFillLinkedin,AiFillYoutube} from 'react-icons/ai'
+import Image from 'next/image';
+import Design from '../public/Design.jpg';
 
 export default function Home() {
   return (
@@ -11,9 +13,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-       <section className=' bg-blue-700 min-h-screen'>
-        <h1>Hello</h1>
+      <main className='bg-white px-10'>
+       <section className='min-h-screen'>
+        <nav className='p-10 mb-12 flex justify-between'>
+          <h1 className=' text-xl font-burtons'>Welcome</h1>
+          <ul className='flex items-center'>
+            <li>
+              <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
+            </li>
+            <li>
+              <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="">Resume</a>
+              </li>
+          </ul>
+        </nav>
+        <div className=' text-center p-10'>
+          <h2 className='text-5xl py-2 text-teal-600'>Martin M</h2>
+          <h3 className='text-2xl py-2'>Software Developer</h3>
+          <p className='text-md py-5 text-amber-700 leading-8'>
+            Javascript - React - SQL - Git - Java 
+          </p>
+        </div>
+        <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+        </div>
+        <div className='relative'>
+          <image src={Design} layout='fill'/>
+        </div>
        </section>
       </main>
     </div>
