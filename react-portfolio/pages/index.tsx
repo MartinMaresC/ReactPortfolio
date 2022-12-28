faa1a850fbc7dc29a4d48f14887569dff3275afa
Martin <martinmarescr@gmail.com>
2022-12-28 16:35:58 -0600

    Update index
    Images and text customized. Profile picture to be added along with more repositories, icons to be changed and contact section too.

--- react-portfolio/pages/index.tsx
+++ react-portfolio/pages/index.tsx
@@ -24,12 +24,13 @@ export default function Home() {
       <main className='bg-white px-10 md:px20 lg:px-40 dark:bg-gray-900'>
        <section className='min-h-screen'>
         <nav className='p-10 mb-12 flex justify-between'>
-          <h1 className=' text-xl font-burtons'>Welcome</h1>
+          <h1 className=' text-xl font-burtons dark:text-white'>Welcome</h1>
           <ul className='flex items-center'>
             <li>
               <BsFillMoonStarsFill 
               onClick={() =>setDarkMode(!darkMode)} 
-              className='cursor-pointer text-2xl' />
+              className='cursor-pointer text-2xl
+              dark:text-white' />
             </li>
             <li>
               <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="">Resume</a>
@@ -38,7 +39,7 @@ export default function Home() {
         </nav>
         <div className=' text-center p-10'>
           <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Martin M</h2>
-          <h3 className='text-2xl py-2 md:text-3xl'>Software Developer</h3>
+          <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Software Developer</h3>
           <p className='text-md py-5 text-amber-700 leading-8 md:text-xl max-w-lg mx-auto'>
             Javascript - React - SQL - Git - Java 
           </p>
@@ -55,9 +56,9 @@ export default function Home() {
 
        <section>
         <div>
-          <h3 className='text-3xl py-1'>Skills</h3>
-          <p className='text-md py-2 leading-8 text-gray-80'>
-            Here you can see some of the personal projects I've created.
+          <h3 className='text-3xl py-1 dark:text-white'>Skills</h3>
+          <p className='text-md py-2 leading-8 text-gray-80 dark:text-white'>
+            Here you can see the spoken and programming languages I know, but also some design tools.
           </p>
         </div>
 
@@ -65,10 +66,10 @@ export default function Home() {
           <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
             <Image src={design} width={100} height={100} />
             <h3 className='text-lg font-medium pt-8 pb-2'>
-              Languages
+              Spoken Languages
             </h3>
             <p className='py-2 max-w-md mx-auto'>
-              Foreign languages I use for personal, academic and professional purposes. 
+              Foreign languages I have used for personal, academic and professional purposes. 
             </p>
             <h4 className='py-4 text-teal-600'>Proficiency</h4>
             <p className='text-gray-800 py-1'>English C1 - Advanced</p>
@@ -78,44 +79,57 @@ export default function Home() {
           <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
             <Image src={code} width={100} height={100} />
             <h3 className='text-lg font-medium pt-8 pb-2'>
-              Project japan
+              Software Development
             </h3>
-            <p className='py-2'>
-              Project description 
+            <p className='py-2 max-w-md mx-auto'>
+               Languages and tools I have use with academic and personal purpose in order to build portfolio.
             </p>
-            <h4 className='py-4 text-teal-600'>Desing tools used</h4>
-            <p className='text-gray-800 py-1'>Gimp</p>
+            <h4 className='py-4 text-teal-600'>Programming languages and resources</h4>
+            <p className='text-gray-800 py-1'>Javascript, React, Next Js</p>
+            <p className='text-gray-800 py-1'>Java, SQL, C</p>
+            <p className='text-gray-800 py-1'>Bootstrap, Css</p>
           </div>
 
           <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
             <Image src={consulting} width={100} height={100} />
             <h3 className='text-lg font-medium pt-8 pb-2'>
-              Project japan
+              Design
             </h3>
-            <p className='py-2'>
-              Project description 
+            <p className='py-2 max-w-md mx-auto'>
+              Desing toolS I have used in order to create wireframes and mockups before. 
             </p>
             <h4 className='py-4 text-teal-600'>Desing tools used</h4>
             <p className='text-gray-800 py-1'>Gimp</p>
+            <p className='text-gray-800 py-1'>Figma</p>
+            <p className='text-gray-800 py-1'>Balsamic</p>
           </div>
         </div>
        </section>
        <section>
         <div>
-          <h3 className='text-3xl py-1'>Portfolio</h3>
-          <p className='text-md py-2 leading-8 text-gray-80'>
-            Here you can see some of the personal projects I've created.
+          <h3 className='text-3xl py-1 dark:text-white'>Portfolio</h3>
+          <p className='text-md py-2 leading-8 text-gray-80 dark:text-white'>
+            Here you can see some of the personal projects I've created. Tools and languages used are described below each image.
           </p>
         </div>
-        <div className='justify-center flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-          <div className='basis-1/3 flex-1'>
+        <div className='justify-center flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap dark:text-white'>
+          <div className='basis-1/3 flex-1 text-center'>
             <Image src={japon} rounded-lg object-cover />
+            <p>
+              HTML - CSS - VANILLA JAVASCRIPT
+            </p>
           </div>
-          <div className='basis-1/3 flex-1'>
+          <div className='basis-1/3 flex-1 text-center'>
             <Image src={refugio} rounded-lg object-cover />
+            <p>
+              HTML - CSS - VANILLA JAVASCRIPT
+            </p>
           </div>
-          <div className='basis-1/3 flex-1'>
+          <div className='basis-1/3 flex-1 text-center'>
             <Image src={carnivoraz} rounded-lg object-cover />
+            <p>
+              HTML - CSS - VANILLA JAVASCRIPT
+            </p>
           </div>
         </div>
        </section>
